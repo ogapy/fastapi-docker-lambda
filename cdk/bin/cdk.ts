8e3e5e76e13d5705ae7406bc08948b14f12890cd
ogapy <kyon6690@gmail.com>
2023-08-11 11:02:51 +0900

    initialize cdk

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+new CdkStack(app, 'CdkStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "ap-northease-1" },
+});
