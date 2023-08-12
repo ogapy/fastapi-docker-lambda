f44b2a517262d9e4ffb374923d66e339d216a501
ogapy <kyon6690@gmail.com>
2023-08-12 15:23:46 +0900

    set GitHub Actions workflow to deploy cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const dockerImage = new ecr_assets.DockerImageAsset(this, 'DockerImage', {
+      directory: '../',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new lambda.DockerImageFunction(this, 'Handler', {
+      code: lambda.DockerImageCode.fromEcr(dockerImage.repository),
+    });
+
+    const api = new apigateway.RestApi(this, 'Api', {
+      restApiName: 'My Service',
+      description: 'This service serves a static website',
+    });
+
+    const getIntegration = new apigateway.LambdaIntegration(handler);
+
+    api.root.addMethod('GET', getIntegration);
   }
 }
